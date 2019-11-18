8a1dd0066088804c532eeeb1ce3fa5cd9bf10aad
vinodnandankar <57030200+vinodnandankar@users.noreply.github.com>
2019-11-18 03:59:02 -0800

    Update TestController.java

--- SpringBootProjects/src/main/java/com/infotech/app/controller/TestController.java
+++ SpringBootProjects/src/main/java/com/infotech/app/controller/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 	
 	@RequestMapping("/")
 	public String getText() {
-		return "-------------------------Hello Vinod--------------------------";
+		return "-------------------------Hello Vinod Nandankar--------------------------";
 		
 	}
 
