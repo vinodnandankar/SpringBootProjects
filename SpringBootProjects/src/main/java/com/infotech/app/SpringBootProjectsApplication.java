d157850a618c365210bcf735e8ede65f908d4504
vinod <vinod@abc.com>
2019-12-13 10:46:38 -0800

    New Changes

--- SpringBootProjects/src/main/java/com/infotech/app/SpringBootProjectsApplication.java
+++ SpringBootProjects/src/main/java/com/infotech/app/SpringBootProjectsApplication.java
@@ -8,7 +8,7 @@ public class SpringBootProjectsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectsApplication.class, args);
-		System.out.println("===============Hello World");
+//		System.out.println("===============Hello World");
 	}
 
 }
--- SpringBootProjects/src/main/java/com/infotech/app/controller/TestController.java
+++ SpringBootProjects/src/main/java/com/infotech/app/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/welcome")
 	public String getText() {
 		return "-------------------------Hello Vinod Nandankar--------------------------";
 		
